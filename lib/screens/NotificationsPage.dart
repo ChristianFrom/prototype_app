999a1f6ff79cc3527e38b4ff9aba27773a871331
Christian From Rasmussen <43375010+ChristianFrom@users.noreply.github.com>
2021-01-09 17:44:08 +0100

    Using db in the cloud

--- lib/screens/NotificationsPage.dart
+++ lib/screens/NotificationsPage.dart
@@ -9,11 +9,18 @@ class _NotificationsPageState extends State<NotificationsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-            child: Text("Notifications Page")
-        ),
-      ),
-    );
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 30.0,
+              vertical: 30.0,
+            ),
+            child: Text("Notifications", style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF211551)),
+            ),
+          ),
+        ));
   }
 }
