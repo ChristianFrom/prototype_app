096ea0d4d64a567487da647bc436c506400ca314
Christian From Rasmussen <43375010+ChristianFrom@users.noreply.github.com>
2021-01-10 02:34:28 +0100

    Revert "Revert "Using db in the cloud""
    This reverts commit f574d78214d4c561578597915ba20af222e4453c.

--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:prototype_app/database_helper.dart';
+import 'package:prototype_app/main.dart';
 import 'package:prototype_app/screens/SensorPage.dart';
 import 'package:prototype_app/widgets.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:prototype_app/models/temperatureSensor.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,86 +12,88 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  DatabaseHelper _dbHelper = DatabaseHelper();
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+  int documentCount;
 
-  @override
-  void initState() {
-    //_dbHelper.insertDummyData();
-    super.initState();
+
+  void countDocs() async {
+    final QuerySnapshot qSnap = await Firestore.instance
+        .collection('SensorTable').get();
+    documentCount = qSnap.docs.length;
+    print("Total docs: " + documentCount.toString());
   }
 
   @override
   Widget build(BuildContext context) {
-    setState(() {
-
-    });
+    setState(() {});
     return Scaffold(
         body: SafeArea(
-          child: Container(
-            width: double.infinity,
-            padding: EdgeInsets.symmetric(
-              horizontal: 24.0,
-            ),
-            color: Color(0xFFF6F6F6),
-            child: Stack(
+      child: Container(
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(
+          horizontal: 24.0,
+        ),
+        color: Color(0xFFF6F6F6),
+        child: Stack(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                        margin: EdgeInsets.only(
-                          top: 32.0,
-                          bottom: 32.0,
-                        ),
-                        child: Center(
-                          child: Text(
-                            "Temperature Sensors",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 30.0),
-                          ),
-                        )),
-                    Expanded(
-                      child: FutureBuilder(
-                        initialData: [],
-                        future: _dbHelper.getTemperatureSensors(),
-                        builder: (context, snapshot) {
+                Container(
+                    margin: EdgeInsets.only(
+                      top: 32.0,
+                      bottom: 32.0,
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Temperature Sensors",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 30.0),
+                      ),
+                    )),
+                Expanded(
+                  child: StreamBuilder<QuerySnapshot>(
+                      stream: FirebaseFirestore.instance.collection("SensorTable").snapshots(),
+                      builder: (BuildContext context,
+                          AsyncSnapshot<QuerySnapshot> snapshot) {
+                        if (snapshot.hasData) {
                           return ScrollConfiguration(
                             behavior: NoGlowScrollBehavior(),
                             child: ListView.builder(
-                              itemCount: snapshot.data.length,
+                              itemCount: snapshot.data.docs.length,
                               itemBuilder: (context, index) {
                                 return InkWell(
                                   onTap: () {
                                     Navigator.push(
                                       context,
                                       MaterialPageRoute(
-                                          builder: (context) =>
-                                              SensorPage(
-                                                sensor: snapshot.data[index],
-                                              )),
+                                          builder: (context) => SensorPage(
+                                            msg: snapshot.data.docs[index],
+                                          )),
                                     ).then((value) {
-                                      setState(() {});
+                                      Home().createState();
                                     });
                                   },
                                   child: SensorCardWidget(
-                                    sensorName: snapshot.data[index].sensorName,
-                                    sensorLocation: snapshot.data[index]
-                                        .sensorLocation,
-                                    sensorGroup: snapshot.data[index]
-                                        .sensorGroup,
+                                    sensorName: snapshot.data.docs[index].id,
+                                    sensorLocation: snapshot.data.docs[index]
+                                        .data()['sensorLocation'],
+                                    sensorGroup: snapshot.data.docs[index]
+                                        .data()['sensorGroup'],
                                   ),
                                 );
                               },
                             ),
                           );
-                        },
-                      ),
-                    )
-                  ],
-                ),
+                        } else return Text("No data available...");
+
+                      }),
+                )
               ],
             ),
-          ),
-        ));
+          ],
+        ),
+      ),
+    ));
   }
 }
