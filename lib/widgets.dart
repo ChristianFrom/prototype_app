4236a480074b21b755ac26e8b42211464e8ca647
Christian From Rasmussen <43375010+ChristianFrom@users.noreply.github.com>
2021-01-10 03:39:57 +0100

    Revert "Revert "Revert "Revert "Real-time temp added""""
    This reverts commit 3b5fc1ab694f02147673fdd8c94d9158a5c4bf0e.

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SensorCardWidget extends StatelessWidget {
   final String sensorName;
@@ -20,30 +21,68 @@ class SensorCardWidget extends StatelessWidget {
       ),
       decoration: BoxDecoration(
           color: Colors.white, borderRadius: BorderRadius.circular(20.0)),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      child: Row(
         children: [
-          Text(
-            sensorName ?? "(Unnamed sensor)",
-            style: TextStyle(
-              color: Color(0xFF211551),
-              fontSize: 22.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-              top: 10,
-            ),
-            child: Text(
-              sensorLocation + " - " + sensorGroup ?? "No location added",
-              style: TextStyle(
-                fontSize: 16.0,
-                color: Color(0xFF86829D),
-                height: 1.5,
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                sensorName ?? "(Unnamed sensor)",
+                style: TextStyle(
+                  color: Color(0xFF211551),
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-          )
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 10,
+                ),
+                child: Text(
+                  sensorLocation + " - " + sensorGroup ?? "No location added",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Color(0xFF86829D),
+                    height: 1.5,
+                  ),
+                ),
+              )
+            ],
+          ),
+          StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection("TemperatureTelemetry")
+                  .where('sensorGroup', isEqualTo: sensorGroup)
+                  .where('sensorLocation', isEqualTo: sensorLocation)
+                  .snapshots(),
+              builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (!snapshot.hasData) {
+                  return Text("No Data");
+                } else {
+                  var temperature = snapshot.data.docs.last.data()['temperature'];
+                  var alarmTriggered = snapshot.data.docs.last.data()['alarmTriggered'];
+                  print(snapshot.data.docs.last.data());
+
+                  return Padding(
+                    padding: const EdgeInsets.only(
+                      left: 120.0
+                    ),
+                    child: Container(
+                      width: 75,
+                      height: 75,
+                      decoration: BoxDecoration(
+                          border: Border.all(width: 3, color: alarmTriggered ? Colors.red : Colors.green),
+                          borderRadius: BorderRadius.all(Radius.circular(200)),
+                          //shape: BoxShape.circle,
+                          color: Colors.white),
+                      child: Center(child: Text(temperature + "°C", style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),)),
+                    ),
+                  );
+                }
+              })
         ],
       ),
     );
@@ -55,7 +94,10 @@ class AlarmTriggeredWidget extends StatelessWidget {
   final double temperature;
   final bool alarmAcknowledged;
 
-  AlarmTriggeredWidget({@required this.timeStamp, @required this.temperature, @required this.alarmAcknowledged});
+  AlarmTriggeredWidget(
+      {@required this.timeStamp,
+      @required this.temperature,
+      @required this.alarmAcknowledged});
 
   @override
   Widget build(BuildContext context) {
@@ -75,22 +117,26 @@ class AlarmTriggeredWidget extends StatelessWidget {
             decoration: BoxDecoration(
                 color: alarmAcknowledged ? Colors.green : Colors.transparent,
                 borderRadius: BorderRadius.circular(6.0),
-                border: alarmAcknowledged ? null : Border.all(
-                  color: Color(0xFF86829D),
-                  width: 1.5,
-                )
-            ),
+                border: alarmAcknowledged
+                    ? null
+                    : Border.all(
+                        color: Color(0xFF86829D),
+                        width: 1.5,
+                      )),
             child: Image(
               image: AssetImage('assets/images/check_icon.png'),
             ),
           ),
           Flexible(
             child: Text(
-              "Alarm Triggered: $timeStamp at $temperature°C " ?? "(No data available...)",
+              "Alarm Triggered: $timeStamp at $temperature°C " ??
+                  "(No data available...)",
               style: TextStyle(
-                color: alarmAcknowledged ? Color(0xFF211511) : Color(0xFF86829D),
+                color:
+                    alarmAcknowledged ? Color(0xFF211511) : Color(0xFF86829D),
                 fontSize: 14.0,
-                fontWeight: alarmAcknowledged ? FontWeight.bold : FontWeight.w500,
+                fontWeight:
+                    alarmAcknowledged ? FontWeight.bold : FontWeight.w500,
               ),
             ),
           ),
@@ -107,6 +153,3 @@ class NoGlowScrollBehavior extends ScrollBehavior {
     return child;
   }
 }
-
-
-
